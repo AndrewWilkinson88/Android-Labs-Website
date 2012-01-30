5fb7f7fdd0a5a218c2fefccc1a1fa83e93e3be1c
awilkins <Andrew_Wilkinson@student.uml.edu>
2012-01-30 12:14:13 -0500

    * Removed Html5Example.html to be replaced with Website.html * Removed Weave.html, not associated with that project anymore * Removed all source code, all code is now hosted on github * Added a github link, and download code link to the top of the project pages * Made it so the links on the left are dynamically loaded, to ensure that all pages have the same number of links

--- build/web/dynamic/LinksDynamic.js
+++ build/web/dynamic/LinksDynamic.js
@@ -6,27 +6,55 @@
                located on the left side of the page.
  */
 
-var school_projects = {
-    "type" : "school_projects",
+var main_links = {
     "links" : {
         "names" : [
-            ["HTML 5 Examples"],
-            ["Weave"],
+            ["Home"],
+            ["About"],
+            ["Blog"],
+            ["Projects"],
+            ["Links"]
+        ] ,
+        "locations" : [
+            ["index.html"],
+            ["About.html"],
+            ["http://androidlabs.blogspot.com/"],
+            ["Projects.html"],
+            ["Links.html"]
+        ]
+      }
+}
+
+var project_links = {
+    "links" : {
+        "names" : [
+            ["This Website"],
             ["TF2 Log Parser"],
             ["L-Systems in OpenTK"]
         ] ,
         "locations" : [
-            ["Html5Example.html"],
-            ["Weave.html"],
+            ["Website.html"],
             ["TF2LogParse.html"],
             ["LSystems.html"]
         ]
-    } 
+      }
 }
-
-var personal_projects = {
+                          
+var other_links = {
+    "links" : {
+        "names" : [
+            ["My Google plus account"],
+            ["My GitHub Profile"],
+            ["My Steam Profile"],
+            ["The Fun-Gineers"],
+            ["Mecha Soup Web Comic"]
+        ] ,
+        "locations" : [
+            ["https://plus.google.com/105792254768354870620/posts"],
+            ["https://github.com/AndrewWilkinson88"],
+            ["http://steamcommunity.com/id/MrAndroid"],
+            ["http://fungineers.net"],
+            ["http://www.mechasoup.com/comic/index.php?date=2005-12-22"]
+        ]
+      }
 }
-
-var extra_links = {
-}
-
--- build/web/scripts/LoadLinks.js
+++ build/web/scripts/LoadLinks.js
@@ -10,29 +10,52 @@
 //duplicate code. Reads the links in from JSON data objects in the 
 //IndexDynamic.js file.
 function initLinks() {
-  placeLinks( school_projects );
+  placeLinks( );
 }
 
 
-function placeLinks(linkSection) {
-
-  //Switch between the five different lessons
-  var loadedContent = linkSection;
+function placeLinks() {
   
   ///<li><a href="Html5Example.html">HTML 5 Examples</a>   </li>
   var strContent = "" ;
   var name = "";
   var location = "";
-
+  
+  strContent += "<h2>Android Labs</h2>";
+  strContent += "<ul>";
+  
   //Cycle through the list of links and concat them as neccessarry 
-  for ( var para = 0 ; para < loadedContent.links.names.length ; para++ ) {
-    name = loadedContent.links.names[para];
-    location = loadedContent.links.locations[para];
-    strContent += "<li class=\"sub\"><a href=\"" + location + "\">" + name + "</a></li>";
+  for ( var i = 0 ; i < main_links.links.names.length ; i++ ) {
+    name = main_links.links.names[i];
+    location = main_links.links.locations[i];
+    strContent += "<li><a class=\"main\" href=\"" + location + "\">" + name + "</a></li>";
+    
+    if( name == "Projects") {
+      strContent += "<li class=\"sub\">";
+      strContent += "<ul>";
+      for ( var j = 0 ; j < project_links.links.names.length ; j++ ) {
+        name = project_links.links.names[j];
+        location = project_links.links.locations[j];
+        strContent += "<li class=\"sub\"><a href=\"" + location + "\">" + name + "</a></li>";
+      }
+      strContent += "</ul>";
+    }
+    
+    if( name == "Links") {
+      strContent += "<li class=\"sub\">";
+      strContent += "<ul>";
+      for ( var k = 0 ; k < other_links.links.names.length ; k++ ) {
+        name = other_links.links.names[k];
+        location = other_links.links.locations[k];
+        strContent += "<li class=\"sub\"><a href=\"" + location + "\">" + name + "</a></li>";
+      } 
+      strContent += "</ul>";
+    }
   }
-  console.log( loadedContent.type );
+  
+  strContent += "</ul>";
+  strContent += "&nbsp;"
   
   //Place the generated page into the website
-  document.getElementById( loadedContent.type ).innerHTML = strContent ;
-  
+  document.getElementById( "links" ).innerHTML = strContent ; 
 }
